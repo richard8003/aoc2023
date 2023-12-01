ff149d3982f872c30b2dd1a655f65318e882d0ed
Richard Johansson <richard.johansson.8003@gmail.com>
2023-12-01 17:06:40 +0100

    foobar

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,58 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var INPUT string
+
+func makeData() [][]string {
+	step_one := strings.Split(strings.TrimRight(INPUT, "\n"), "\n")
+	data := returnOnlyNumbersFromRows(step_one)
+	return data
+}
+
+func returnOnlyNumbersFromRows(rows []string) [][]string {
+	var int_data [][]string
+
+	for _, el := range rows {
+		var line []string
+		for _, x := range strings.Split(el, "") {
+			_, err := strconv.Atoi(x)
+			if err != nil {
+				continue
+			}
+			line = append(line, x)
+		}
+		int_data = append(int_data, line)
+	}
+
+	return int_data
+}
+
+func partOne(data [][]string) {
+	var sum int
+
+	for _, n := range data {
+
+		number_string := n[0] + n[len(n)-1]
+
+		num, err := strconv.Atoi(number_string)
+		if err != nil {
+			fmt.Println("Fubar!!!!")
+		}
+		sum += num
+
+	}
+	fmt.Println(sum)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	data := makeData()
+
+	partOne(data)
 }
