a00dcc00f758ce4cdae47aa12b3bea2cfe1bc878
Richard Johansson <richard.johansson.8003@gmail.com>
2023-12-04 18:48:35 +0100

    foobar

--- day1/main.go
+++ day1/main.go
@@ -61,24 +61,24 @@ func main() {
 	// fubar := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "zero"}
 	// fmt.Println(fubar)
 
-	// fmt.Println(data)
-
-	for _, row := range data {
-		// fmt.Println(row)
-		row = strings.ReplaceAll(row, "one", "1")
-		row = strings.ReplaceAll(row, "two", "2")
-		row = strings.ReplaceAll(row, "three", "3")
-		row = strings.ReplaceAll(row, "four", "4")
-		row = strings.ReplaceAll(row, "five", "5")
-		row = strings.ReplaceAll(row, "six", "6")
-		row = strings.ReplaceAll(row, "seven", "7")
-		row = strings.ReplaceAll(row, "eight", "8")
-		row = strings.ReplaceAll(row, "nine", "9")
-		row = strings.ReplaceAll(row, "zero", "0")
-		fmt.Println(row)
-	}
-
 	fmt.Println(data)
+
+	// for _, row := range data {
+	// fmt.Println(row)
+	// row = strings.ReplaceAll(row, "one", "1")
+	// row = strings.ReplaceAll(row, "two", "2")
+	// row = strings.ReplaceAll(row, "three", "3")
+	// row = strings.ReplaceAll(row, "four", "4")
+	// row = strings.ReplaceAll(row, "five", "5")
+	// row = strings.ReplaceAll(row, "six", "6")
+	// row = strings.ReplaceAll(row, "seven", "7")
+	// row = strings.ReplaceAll(row, "eight", "8")
+	// row = strings.ReplaceAll(row, "nine", "9")
+	// row = strings.ReplaceAll(row, "zero", "0")
+	// fmt.Println(row)
+	// }
+
+	// fmt.Println(data)
 
 	// test := "jagHarEn"
 	//
